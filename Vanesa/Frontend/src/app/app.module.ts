d7c960a49dfcb7b5f170c26f97a7cf8d7ddb27de
evha1414 <vanesasolterman@gmail.com>
2022-10-21 13:18:09 -0300

    Frontend

--- Vanesa/Frontend/src/app/app.module.ts
+++ Vanesa/Frontend/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { BannerComponent } from './components/banner/banner.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-import { RedesComponent } from './components/redes/redes.component';
+
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -23,16 +26,18 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     LogoAPComponent,
     AcercaDeComponent,
     EducacionComponent,
-    RedesComponent,
     ExperienciaComponent,
     FooterComponent,
     HysComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
